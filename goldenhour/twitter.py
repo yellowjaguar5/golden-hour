83556be1d6bb6dcd88dd37f69a5ad8a6355a4e4d
rush340 <rush340@gmail.com>
2017-02-11 23:05:33 -0800

    use chunked upload

--- goldenhour/twitter.py
+++ goldenhour/twitter.py
@@ -9,19 +9,25 @@ from twitter.twitter_utils import parse_media_file
 def load_credentials():
     with open('twitter_secrets.yaml') as twitter_conf_file:
         conf = yaml.load(twitter_conf_file.read())
-    # TODO only return credential keys from yaml
-    return conf
+    return {
+        'consumer_key': conf['consumer_key'],
+        'consumer_secret': conf['consumer_secret'],
+        'access_token_key': conf['access_token_key'],
+        'access_token_secret': conf['access_token_secret'],
+    }
 
 
 def post_update(text, media=None):
     print('posting to twitter (status_text: {}, media: {})'.format(text, media))
     credentials = load_credentials()
-    print(credentials)
     api = twitter.Api(**credentials)
-    print(api.VerifyCredentials())
+
+    media_id = None
     if media:
-        print(parse_media_file(media))
-    api.PostUpdate(text, media)
+        with open(media, 'rb') as mediafile:
+            media_id = api.UploadMediaChunked(mediafile)
+
+    api.PostUpdate(text, media=media_id)
 
 
 def main():
